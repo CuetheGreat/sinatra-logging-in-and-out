37588c8f148a1a2136839cfa33089940597dbff4
Clay Stewart <c.stewart89@gmail.com>
2021-09-27 14:44:32 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,18 +1,19 @@
 source 'http://rubygems.org'
 
-gem 'sinatra', '~> 2.0'
 gem 'activerecord', '~> 5.1', '>= 5.1.5'
-gem 'sinatra-activerecord', '~> 2.0', '>= 2.0.13'
+gem 'pry'
 gem 'rake'
 gem 'require_all'
+gem 'shotgun'
+gem 'sinatra', '~> 2.0'
+gem 'sinatra-activerecord', '~> 2.0', '>= 2.0.13'
 gem 'sqlite3', '~>1.3.6'
 gem 'thin'
-gem 'shotgun'
-gem 'pry'
 
 group :test do
-  gem 'rspec'
   gem 'capybara'
-  gem 'rack-test'
   gem 'database_cleaner'
+  gem 'rack-test'
+  gem 'rspec'
+  gem 'solargraph'
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
+  # ...
   configure do
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :views, proc { File.join(root, '../views/') }
     enable :sessions unless test?
-    set :session_secret, "secret"
+    set :session_secret, 'secret'
   end
 
   get '/' do
@@ -11,17 +15,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params['username'], password: params['password'])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
-
